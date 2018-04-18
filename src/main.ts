a5168f3c836055f52c5a3c98a00bbe3133346fca
chris <yeditor@gmail.com>
2018-04-18 20:18:02 +0800

    小記 prototype

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+export {};
+declare global {
+    interface Number {
+        mm(): number;
+    }
+}
+Number.prototype.mm = function () {
+    return this/1000 ;
+}
+console.log(1..mm()) ;
+
+
 if (environment.production) {
   enableProdMode();
 }
